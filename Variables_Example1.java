a6c56e9a790a11183135d44502fb9134bb566383
naveen988576 <119918503+naveen988576@users.noreply.github.com>
2024-03-04 19:15:10 +0530

    Add files via upload

---
+++ Variables_Example1.java
@@ -0,0 +1,17 @@
+package naveen.java;
+
+public class Variables_Example1 {
+
+	public static void main(String[] args) {
+		int number1=12,number2=13,number3=11,avg,sum,sub,mul;
+		sum=number1+number2+number3;
+		sub=number1-number2-number3;
+		mul=number1*number2*number3;
+		avg=number1+number2+number3/3;
+		System.out.println("sum of three numbers is :"+sum);
+		System.out.println("sub of three numbers is :"+sub);
+		System.out.println("mul of three numbers is :"+mul);
+		System.out.println("avg of three numbers is :"+avg);
+	}
+
+}
