e4949dd043831875cefac2e94e01bb0b679f0295
naveen988576 <119918503+naveen988576@users.noreply.github.com>
2024-03-04 16:49:35 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package naveen.java.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome To Java World");
+
+	}
+
+}
