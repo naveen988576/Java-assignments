e9b3f074722ee8a9c69d012a36acff25cb94a244
naveen988576 <119918503+naveen988576@users.noreply.github.com>
2024-03-05 00:17:15 +0530

    Add files via upload

---
+++ Employee_Info.java
@@ -0,0 +1,28 @@
+package naveen.java;
+
+import java.util.Scanner;
+
+public class Employee_Info 
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the employee id:");
+		int eid=sc.nextInt();
+		//System.out.println("enter the employee name:");
+		//string ename=sc.next();
+		System.out.println("enter the employee salary:");
+		double esalary=sc.nextDouble();
+		System.out.println("enter the employee address:");
+		int eaddress=sc.nextInt();
+		System.out.println("enter the employee contact:");
+		long econtact=sc.nextInt();
+		System.out.println("enter the employee gender:");
+		int egender=sc.nextInt();
+		
+		
+		
+
+	}
+
+}
