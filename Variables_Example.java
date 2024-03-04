27e319b2e5b9217ce38eb5e35df8c7a9b1e4da47
naveen988576 <119918503+naveen988576@users.noreply.github.com>
2024-03-04 17:14:32 +0530

    Add files via upload

---
+++ Variables_Example.java
@@ -0,0 +1,19 @@
+package naveen.java;
+
+public class Variables_Example {
+
+	public static void main(String[] args) {
+		byte number1=12;
+		byte number2=12;
+		if(number1==number2)
+		{
+			System.out.println("number1 and number2 values are equal");
+		}
+		else
+		{
+			System.out.println("number1 and number2 values are not equal");
+		}
+
+	}
+
+}
